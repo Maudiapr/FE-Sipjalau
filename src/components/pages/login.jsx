96de788494b2937786e3782f8f30a9d794c3ad05
Maudi <maudinaapriliani146@gmail.com>
2026-07-11 22:56:58 +0700

    style login

--- src/components/pages/login.jsx
+++ src/components/pages/login.jsx
@@ -1,19 +1,31 @@
 import { AuthLayout } from "../layout/authLayout"
-import { InputField } from "../elements/inputField"
+import { useNavigate } from "react-router-dom"
 
 export const Login = () => {
+    const navigate = useNavigate()
+    
+    const handleLogin = () => {
+        navigate('/')
+    }
     return(
         <AuthLayout title="Login" desc="Welcome to Happy Wash Laundry">
-            <InputField placeholder="Ucup Surucup" label="Username" type="text" id="username" name="username" htmlFor="username" classNameI="rounded-lg h-10"/>
-            <InputField placeholder="Ucup" label="Name" type="text" id="name" name="name" htmlFor="name" classNameI="rounded-lg h-10"/>
-    
-            <select name="role" className="border border-gray-300 p-1 outline-none focus:border-blue-600 rounded-lg text-sm h-10">
+            <label htmlFor="username">
+                <p>Username</p>
+                <input type="text" name="userName" id="username" placeholder="Ucup Surucup" className="border border-gray-300 p-2.5 w-full rounded-sm"/>
+            </label>
+
+            <select name="role" className="border border-gray-300 p-2.5 rounded-sm h-10">
                 <option value="">Pilih Role</option>
                 <option value="kasir">Kasir</option>
                 <option value="pekerja">Pekerja</option>
             </select>
 
-            <InputField placeholder="********" label="Password" type="password" id="password" name="password" htmlFor="password" classNameI="rounded-lg h-10 placeholder:text-start"/>
+            <label htmlFor="pass">
+                <p>Password</p>
+                <input type="text" name="password" id="pass" placeholder="**********" className="border border-gray-300 p-2.5 w-full rounded-sm"/>
+            </label>
+
+            <button onClick={handleLogin} className='bg-blue-900 text-white h-10 rounded-lg hover:bg-blue-700'>Login</button>
         </AuthLayout>
     )
 }
